a6ea4843e12625282d0fa6e30a9d8550e91c28a6
Marcel Herr <mrclhrr@gmail.com>
2024-06-27 11:53:02 +0200

    add array basics challenge

--- project-3/src/main/java/ArrayBasics.java
+++ project-3/src/main/java/ArrayBasics.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class ArrayBasics {
+import java.util.Arrays;
+
+public class ArrayBasics {
+
+    public static void main(String[] args) {
+
+        // Aufgabe 1
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            numbers[i] = i + 1;
+        }
+        System.out.println("Erstes Array: " + Arrays.toString(numbers));
+
+        // Aufgabe 2
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            sum += numbers[i];
+        }
+        System.out.println("Summe aller Zahlen: " + sum);
+
+        // Aufgabe 3
+        int largestNumber = 0;
+
+        for (int number : numbers) {
+
+            if (number > largestNumber)
+                largestNumber = number;
+        }
+        System.out.println("Groeste Zahl: " + largestNumber);
+
+
+        //Aufgabe 4
+        int[] secondNumbers = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            secondNumbers[i] = i + 11;
+        }
+
+        System.out.println("Zweites Array: " + Arrays.toString(secondNumbers));
+
+
+        //Aufabe 5
+
+        int[] combinedNumbers = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            combinedNumbers[i] = numbers[i] + secondNumbers[i];
+        }
+
+        System.out.println("Summer der Komibinierten Arrays: " + Arrays.toString(combinedNumbers));
+    }
+
 }
