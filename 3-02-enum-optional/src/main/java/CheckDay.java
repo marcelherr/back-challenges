3fb1f7a212e8b87b31761aa39152fab2593ab1ec
Marcel Herr <mrclhrr@gmail.com>
2024-07-11 11:29:13 +0200

    add solution step 1 and 2

---
+++ 3-02-enum-optional/src/main/java/CheckDay.java
@@ -0,0 +1,18 @@
+public class CheckDay {
+
+    public static String checkDay(DaysOfWeek day) {
+        switch (day) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+            case FRIDAY:
+                return day.name();
+            case SATURDAY:
+            case SUNDAY:
+                return "Weekend";
+            default:
+                throw new AssertionError(day.name());
+        }
+    }
+}
---
+++ 3-02-enum-optional/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(CheckDay.checkDay(DaysOfWeek.THURSDAY));
+        System.out.println(CheckDay.checkDay(DaysOfWeek.SATURDAY));
+
+        System.out.println();
+    }
+}
