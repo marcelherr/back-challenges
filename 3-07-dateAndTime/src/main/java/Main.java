6dcfa38217c2076cea3b50a19868b6872a5a6c05
Marcel Herr <mrclhrr@gmail.com>
2024-07-16 11:44:14 +0200

    solution step 2 and step 3

--- 3-07-dateAndTime/src/main/java/Main.java
+++ 3-07-dateAndTime/src/main/java/Main.java
@@ -2,8 +2,21 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Step1
         LocalDateTime currentTime = LocalDateTime.now();
         System.out.println(currentTime);
-        System.out.println(currentTime);
+
+        //Step2
+        LocalDateTime futureTime = currentTime.plusWeeks(2);
+        System.out.println(futureTime);
+
+        //Step3
+        if (currentTime.isBefore(futureTime)) {
+            System.out.println("The current Time is before.");
+        } else if (currentTime.isAfter(futureTime)) {
+            System.out.println("The current Time is after.");
+        }
+
     }
 }
