1727b6133df9716a560514cad659262d48f729fc
Marcel Herr <mrclhrr@gmail.com>
2024-07-16 11:54:08 +0200

    solution step 4

--- 3-07-dateAndTime/src/main/java/Main.java
+++ 3-07-dateAndTime/src/main/java/Main.java
@@ -1,15 +1,23 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         //Step1
         LocalDateTime currentTime = LocalDateTime.now();
-        System.out.println(currentTime);
+        String formattedDate = currentTime.format(formatter);
+
+        System.out.println(formattedDate);
 
         //Step2
         LocalDateTime futureTime = currentTime.plusWeeks(2);
-        System.out.println(futureTime);
+        String formattedFuture = futureTime.format(formatter);
+
+        System.out.println(formattedFuture);
 
         //Step3
         if (currentTime.isBefore(futureTime)) {
@@ -17,6 +25,13 @@ public class Main {
         } else if (currentTime.isAfter(futureTime)) {
             System.out.println("The current Time is after.");
         }
+
+        //Step 4
+        LocalDateTime startTime = LocalDateTime.now();
+        LocalDateTime endTime = LocalDateTime.now().plusHours(2);
+
+        long difference = ChronoUnit.HOURS.between(startTime, endTime);
+        System.out.println("The difference is: " + difference);
 
     }
 }
