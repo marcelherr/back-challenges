c4c2a35558abe16176ae0211b3835b772c077372
Marcel Herr <mrclhrr@gmail.com>
2024-07-18 11:40:34 +0200

    fixed solution step 3

--- 4-02-Spring-FirstControllers/src/main/java/org/example/firstcontrollers/Hello.java
+++ 4-02-Spring-FirstControllers/src/main/java/org/example/firstcontrollers/Hello.java
@@ -1,8 +1,6 @@
 package org.example.firstcontrollers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -13,8 +11,8 @@ public class Hello {
         return "Hello World";
     }
 
-    @GetMapping("/hello/john")
-    public String helloSomeone() {
-        return "Hello, John!";
+    @PostMapping("/hello/{name}")
+    public String helloSomeone(@PathVariable String name) {
+        return "Hello, " + name + " !";
     }
 }
