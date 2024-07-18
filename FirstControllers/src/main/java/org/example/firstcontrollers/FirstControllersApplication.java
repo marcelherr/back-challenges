09cc9c6b42574b4517e53650e3709a7eb8d69428
Marcel Herr <mrclhrr@gmail.com>
2024-07-18 11:20:26 +0200

    Init First Controller Porject and solution step 1 and step 2

---
+++ FirstControllers/src/main/java/org/example/firstcontrollers/FirstControllersApplication.java
@@ -0,0 +1,15 @@
+package org.example.firstcontrollers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FirstControllersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstControllersApplication.class, args);
+    }
+
+
+}
