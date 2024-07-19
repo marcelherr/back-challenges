5da3de93588025b9881e0aea86fc483f1beaeab6
Marcel Herr <mrclhrr@gmail.com>
2024-07-19 16:41:39 +0200

    add db connection

--- 4-04-springData/src/main/java/org/example/asterixapi/AsterixController.java
+++ 4-04-springData/src/main/java/org/example/asterixapi/AsterixController.java
@@ -4,27 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class AsterixController {
 
+    private final AsterixRepo asterixRepo;
+
+    public AsterixController(AsterixRepo asterixRepo) {
+        this.asterixRepo = asterixRepo;
+    }
+
     @GetMapping("/characters")
     public List<Character> getAllCharacters() {
-        return List.of(
-                new Character("1", "Asterix", 35, "Krieger"),
-                new Character("2", "Obelix", 35, "Lieferant"),
-                new Character("3", "Miraculix", 60, "Druide"),
-                new Character("4", "Majestix", 60, "Häuptling"),
-                new Character("5", "Troubadix", 25, "Barden"),
-                new Character("6", "Gutemine", 35, "Häuptlingsfrau"),
-                new Character("7", "Idefix", 5, "Hund"),
-                new Character("8", "Geriatrix", 70, "Rentner"),
-                new Character("9", "Automatix", 35, "Schmied"),
-                new Character("10", "Grockelix", 35, "Fischer")
-        );
-
+        List<Character> allCharacters = asterixRepo.findAll();
+        return allCharacters;
     }
 
 }
--- 4-04-springData/src/main/java/org/example/asterixapi/Character.java
+++ 4-04-springData/src/main/java/org/example/asterixapi/Character.java
@@ -1,10 +1,13 @@
 package org.example.asterixapi;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("characters")
 public record Character(
         String id,
         String name,
         int age,
         String profession
 ) {
-    
+
 }
