d47d0dcbd835b1161747fa6dd3e8da2991b03d05
Marcel Herr <mrclhrr@gmail.com>
2024-07-23 12:06:47 +0200

    add getAllCharacters and postCharacter integration test

--- 4-04-springData/src/test/java/org/example/asterixapi/AsterixIntegrationTest.java
+++ 4-04-springData/src/test/java/org/example/asterixapi/AsterixIntegrationTest.java
@@ -4,8 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.awt.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,9 +27,42 @@ public class AsterixIntegrationTest {
     @Test
     @DirtiesContext
     void getAllCharacters() throws Exception {
+
         //Given
-        asterixRepo.save(new Character("1","Tester1", 22, "Testing" ));
+        asterixRepo.save(new Character("1", "Tester1", 22, "Testing"));
+
         //When
-        mockMvc.perform
+        mockMvc.perform(get("/api/characters"))
+
+                //Then
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [
+                        {
+                        "name":"Tester1",
+                        "age":22,
+                        "profession":"Testing"
+                        }
+                        ]
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    void postCharacter() throws Exception {
+        //Given
+        //When
+        mockMvc.perform(post("/api/characters")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                   "name":"Tester2",
+                                                        "age":33,
+                                                        "profession":"Testing2"
+
+                                }
+                                """))
+                .andExpect(jsonPath("$.id").exists());
+
     }
 }
