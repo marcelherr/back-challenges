f78c8d37d28c6650d16fbfdea2d4f21138212612
Marcel Herr <mrclhrr@gmail.com>
2024-08-06 17:01:20 +0200

    squared list

--- 5-04-typescript-function/src/main.ts
+++ 5-04-typescript-function/src/main.ts
@@ -26,3 +26,14 @@ const someNumber = numbers.some(n => n > 10)
 
 console.log(someNumber)
 
+//Bonus
+
+const list = [15, 6, 3213, 9, 0, 12, 8464, 1, 1264, 481, 186, 1031, 194];
+
+const sortedList = list.sort((a, b) => b - a)
+
+console.log(sortedList)
+
+const squaredList = sortedList.map(n => n * n)
+
+console.log(squaredList)
